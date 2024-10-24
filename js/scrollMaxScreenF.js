fb82666aabf93b5f9d27f11ff30ea0902219e1cd
zhangyang <1554267532@qq.com>
2024-10-24 22:45:44 +0800

    Site updated: 2024-10-24 22:45:20

---
+++ js/scrollMaxScreenF.js
@@ -0,0 +1,59 @@
+(function() {
+    const scrollFnMaxScreenF = function () {
+        const $rightside = document.getElementById('rightside')
+        const innerHeight = window.innerHeight + 56
+
+        // find the scroll direction
+        function scrollDirection (currentTop) {
+            const result = currentTop > initTop // true is down & false is up
+            initTop = currentTop
+            return result
+        }
+
+        let initTop = 0
+        let isChatShow = true
+        const $header = document.getElementById('page-header')
+        const isChatBtnHide = typeof chatBtnHide === 'function'
+        const isChatBtnShow = typeof chatBtnShow === 'function'
+
+        window.scrollCollect = () => {
+            return btf.throttle(function (e) {
+                const currentTop = window.scrollY || document.documentElement.scrollTop
+                const isDown = scrollDirection(currentTop)
+                if (currentTop > 56) {
+                    if (isDown) {
+                        if ($header.classList.contains('nav-visible')) $header.classList.remove('nav-visible')
+                        if (isChatBtnShow && isChatShow === true) {
+                            chatBtnHide()
+                            isChatShow = false
+                        }
+                    } else {
+                        if (!$header.classList.contains('nav-visible')) $header.classList.add('nav-visible')
+                        if (isChatBtnHide && isChatShow === false) {
+                            chatBtnShow()
+                            isChatShow = true
+                        }
+                    }
+                    $header.classList.add('nav-fixed')
+                    if (window.getComputedStyle($rightside).getPropertyValue('opacity') === '0') {
+                        $rightside.style.cssText = 'opacity: 0.8; transform: translateX(-58px)'
+                    }
+                } else {
+                    if (currentTop === 0) {
+                        $header.classList.remove('nav-fixed', 'nav-visible')
+                    }
+                    $rightside.style.cssText = "opacity: ''; transform: ''"
+                }
+
+                if (document.body.scrollHeight <= innerHeight) {
+                    $rightside.style.cssText = 'opacity: 0.8; transform: translateX(-58px)'
+                }
+            }, 200)()
+        }
+
+        window.addEventListener('scroll', scrollCollect)
+    }
+
+    document.addEventListener('pjax:complete', scrollFnMaxScreenF);
+    document.addEventListener('DOMContentLoaded', scrollFnMaxScreenF);
+})();
